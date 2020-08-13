6e21309504a20cc578158cfa2efb37a3a6b48d1c
darcipons <darcimarie33@gmail.com>
2020-08-13 17:02:36 -0400

    set up redux and reducers

---
+++ src/playground/destructuring.js
@@ -0,0 +1,43 @@
+// OBJECT DESTRUCTORING
+
+// const person = {
+//   name: 'Darci',
+//   age: 20,
+//   location: {
+//     city: 'Miami',
+//     temp: 90
+//   }
+// };
+
+// const { name = 'Anon' , age } = person
+
+// console.log(`${name} is ${age}.`)
+
+// const { city, temp: temperature } = person.location;
+
+// if (city && temperature){
+//   console.log(`It's ${temperature} in ${city}`)
+// } 
+
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     //name: 'Penguin'
+//   }
+// }
+
+// const { name: publisherName = 'Self-Made'  } = book.publisher
+
+// console.log(publisherName)
+
+
+
+
+// ARRAY DESTRUCTURING
+
+// const address = ['1299 S Juniper Street', 'Miami', 'Florida', '19154'];
+
+// const [street, city, state, zip] = address;
+
+// console.log(`You are in ${city} ${state}`);
